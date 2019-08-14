a6aeac8b4a8a0aec0c0ca3912d970a2732390ffc
David Clark <dclark@greenchef.com>
2019-08-14 09:47:15 -0600

    Throws an error when mongo connection env vars are missing.

--- initializers/utils.js
+++ initializers/utils.js
@@ -1,4 +1,8 @@
 const getMongoConnectionString = () => {
+	if (!(process.env.MONGODB_URI || process.env.MONGO_URI)) {
+		throw Error('No mongo connection string detected! One of (MONGO_URI, [MONGODB_URI, MONGODB_NAME, MONGODB_OPTIONS]) must be provided.');
+	}
+
 	return process.env.MONGODB_URI
 		? `${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}${process.env.MONGODB_OPTIONS}`
 		: process.env.MONGO_URI;
