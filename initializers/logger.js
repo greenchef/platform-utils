4d9ba7ac84d79b9af1c89700746111f0d6e57426
David Clark <dclark@greenchef.com>
2019-08-14 09:43:22 -0600

    Throws an error if LOGGER_NAME environment variable is missing.

--- initializers/logger.js
+++ initializers/logger.js
@@ -1,7 +1,11 @@
 const bunyan = require('bunyan');
 
+if (!process.env.LOGGER_NAME) {
+	throw Error('LOGGER_NAME environment variable not provided for logger initializer.');
+}
+
 const logger = bunyan.createLogger({
-	name: process.env.LOGGER_NAME || 'unnamed',
+	name: process.env.LOGGER_NAME,
 	level: process.env.LOG_LEVEL || 'debug',
 	src: true,
 	stream: process.stdout,
