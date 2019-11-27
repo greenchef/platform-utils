fba231e301d4a78321d7d70a0762a64e20dd662d
Tom Conchie <tconchie@greenchef.com>
2019-11-27 10:21:03 -0700

    Fixes reliance on strict query by deleting params from req/body/query when stripping off search options

--- mongoose/index.js
+++ mongoose/index.js
@@ -101,26 +101,44 @@ const searchOptions = (req) => {
 	} else if (req.limit || (req.query && req.query.limit) || (req.body && req.body.limit)) {
 		options.limit = req.limit || req.query.limit || req.body.limit;
 	}
+	delete req.limit;
+	if (req.query) delete req.query.limit;
+	if (req.body) delete req.body.limit;
 
 	if (req.populate || (req.query && req.query.populate) || (req.body && req.body.populate)) {
 		options.populate = req.populate || req.query.populate || req.body.populate;
 	}
+	delete req.populate;
+	if (req.query) delete req.query.populate;
+	if (req.body) delete req.body.populate;
 
 	if (req.projection || (req.query && req.query.projection) || (req.body && req.body.projection)) {
 		options.projection = req.projection || req.query.projection || req.body.projection;
 	}
+	delete req.projection;
+	if (req.query) delete req.query.projection;
+	if (req.body) delete req.body.projection;
 
 	if (req.skip || (req.query && req.query.skip) || (req.body && req.body.skip)) {
 		options.skip = req.skip || req.query.skip || req.body.skip;
 	}
+	delete req.skip;
+	if (req.query) delete req.query.skip;
+	if (req.body) delete req.body.skip;
 
 	if (req.sortOrder || (req.query && req.query.sortOrder) || (req.body && req.body.sortOrder)) {
 		options.sortOrder = req.sortOrder || req.query.sortOrder || req.body.sortOrder;
 	}
+	delete req.sortOrder;
+	if (req.query) delete req.query.sortOrder;
+	if (req.body) delete req.body.sortOrder;
 
 	if (req.sortProperty || (req.query && req.query.sortProperty) || (req.body && req.body.sortProperty)) {
 		options.sortProperty = req.sortProperty || req.query.sortProperty || req.body.sortProperty;
 	}
+	delete req.sortProperty;
+	if (req.query) delete req.query.sortProperty;
+	if (req.body) delete req.body.sortProperty;
 
 	return options;
 };
