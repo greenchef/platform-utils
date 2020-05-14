7f81e084a4dcc339563e215f0a1bf2e2482b9a01
jbc5099 <jbc5099@gmail.com>
2020-05-14 17:05:17 -0600

    [FWK-185] updates seed index

--- seed/index.js
+++ seed/index.js
@@ -1,7 +1,12 @@
 const CollectionSeeder = require("./collection-seeder");
 const Resolver = require("./resolver");
+const { buildDep, copyData, fetchData, publishData } = require('./seed-config.lib');
 
 module.exports = {
+	buildDep,
 	CollectionSeeder,
+	copyData,
+	fetchData,
+	publishData,
 	Resolver
 }
