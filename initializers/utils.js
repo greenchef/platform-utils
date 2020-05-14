51a8597939a3efb35868b3bfed6fea468b5e48be
Tom Conchie <tconchie@greenchef.com>
2020-05-14 10:42:28 -0600

    [fwk-153] Creates exportable connections to mongoose

--- initializers/utils.js
+++ initializers/utils.js
@@ -1,3 +1,34 @@
+const mongoose = require('mongoose');
+
+const logger = require('./logger');
+
+const createMongooseConnection = connectionString => {
+	mongoose.Promise = global.Promise;
+	try {
+		const connectWithRetry = () => {
+			mongoose.Promise = global.Promise;
+
+			const isMongoose5 = mongoose.version && mongoose.version.startsWith('5');
+			let options = { useNewUrlParser: true };
+			if (!isMongoose5) options = { ...options, useMongoClient: true };
+
+			return mongoose.connect(connectionString, options, (err) => {
+				if (err) {
+					logger.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+					setTimeout(connectWithRetry, 5000);
+				} else {
+					logger.info('mongoose connected!!!!');
+				}
+			});
+		};
+		connectWithRetry();
+	} catch (e) {
+		logger.error('unable to connect to mongoose: %s', e.stack);
+		process.exit(1);
+	}
+	return mongoose;
+}
+
 const getMongoConnectionString = () => {
 	if (!(process.env.MONGODB_URI || process.env.MONGO_URI)) {
 		throw Error('No mongo connection string detected! One of (MONGO_URI, [MONGODB_URI, MONGODB_NAME, MONGODB_OPTIONS]) must be provided.');
@@ -14,6 +45,7 @@ const isProdDB = () => {
 }
 
 module.exports = {
+	createMongooseConnection,
 	getMongoConnectionString,
 	isProdDB,
 }
