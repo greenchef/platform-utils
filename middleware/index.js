bd7d26c0212a3b44beaf5c3899bb17295edcadf6
BriannaGreene <hellobrigreene@gmail.com>
2020-06-26 13:49:38 -0600

    [FWK-206] create super user token that can take permissions array

--- middleware/index.js
+++ middleware/index.js
@@ -2,10 +2,12 @@ const asyncWrap = require('./async.middleware');
 const httpErrorHandler = require('./http-error-handler.middleware');
 const jwtDecode = require('./jwt-decode.middleware');
 const permit = require('./permit.middleware');
+const createUserToken = require('./create-user-token.middleware');
 
 module.exports = {
 	asyncWrap,
 	httpErrorHandler,
 	jwtDecode,
 	permit,
+	createUserToken,
 };
