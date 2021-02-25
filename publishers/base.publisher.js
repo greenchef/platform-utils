f7489534c4ce58874de201450943f45fde794b83
Brady-Conn <jbc5099@gmail.com>
2021-02-25 14:02:15 -0700

    [FWK-622] fix for menu user invoiced publisher

--- publishers/base.publisher.js
+++ publishers/base.publisher.js
@@ -7,54 +7,55 @@ const SNS = new AWS.SNS({ apiVersion: '2010-03-31', endpoint: process.env.SNS_EN
 const { joi: Joi, log } = require('../initializers');
 
 class BasePublisher {
-  constructor(topic, schema) {
-    if (!topic) throw new Error('Topic name is required');
-    this.logger = log.createLogger({ group: this.constructor.name, publisherName: this.constructor.name });
-    this.ready = false;
-    this.schema = schema;
-    this.topic = process.env.APP_CLUSTER ? `${process.env.APP_CLUSTER}-${topic}` : `local-${topic}`;
-    this.topicArn = null;
-  }
+	constructor(topic, schema) {
+		if (!topic) throw new Error('Topic name is required');
+		this.logger = log.createLogger({ group: this.constructor.name, publisherName: this.constructor.name });
+		this.ready = false;
+		this.schema = schema;
+		this.topic = process.env.APP_CLUSTER ? `${process.env.APP_CLUSTER}-${topic}` : `local-${topic}`;
+		this.topicArn = null;
+	}
 
-  validate(data) {
-    if (!this.schema) return data;
-    const { error, value } = Joi.validate(data, this.schema);
-    if (error) throw error;
-    return value;
-  }
+	validate(data) {
+		if (!this.schema) return data;
+		const { error, value } = Joi.validate(data, this.schema);
+		if (error) throw error;
+		return value;
+	}
 
-  getTopic() {
-    return this.topic;
-  }
+	getTopic() {
+		return this.topic;
+	}
 
-  async publish(data = {}) {
-    const message = JSON.stringify(this.validate(data));
+	async publish(data = {}) {
+		// extra parsing and stringify to account for nested mongo object id's and other oddities
+		const message = JSON.stringify(this.validate(JSON.parse(JSON.stringify(data))));
 
-    // Create publish parameters
-    try {
-      const params = {
-        Message: message, /* required */
-        TopicArn: this.topicArn,
-      };
-      // Create promise and SNS service object
-      const publisher = SNS.publish(params).promise();
-      const snsResponse = await publisher;
-      return snsResponse;
-    } catch (err) {
-      this.logger.error(err, { supplementalMessage: `Error publishing message for topic ${this.topic}` });
-      throw err;
-    }
-  }
+		// Create publish parameters
+		try {
+			const params = {
+				Message: message, /* required */
+				TopicArn: this.topicArn,
+			};
+			// Create promise and SNS service object
+			const publisher = SNS.publish(params).promise();
+			const snsResponse = await publisher;
+			return snsResponse;
+		} catch (err) {
+			this.logger.error(err, { supplementalMessage: `Error publishing message for topic ${this.topic}` });
+			throw err;
+		}
+	}
 
-  async connect() {
-    try {
-      const snsCreateResponse = await SNS.createTopic({ Name: this.topic }).promise();
-      this.topicArn = snsCreateResponse.TopicArn;
-    } catch (err) {
-      this.logger.error(err, { supplementalMessage: `Error creating topic ${this.topic}` });
-      throw err;
-    }
-  }
+	async connect() {
+		try {
+			const snsCreateResponse = await SNS.createTopic({ Name: this.topic }).promise();
+			this.topicArn = snsCreateResponse.TopicArn;
+		} catch (err) {
+			this.logger.error(err, { supplementalMessage: `Error creating topic ${this.topic}` });
+			throw err;
+		}
+	}
 }
 
 module.exports = BasePublisher;
